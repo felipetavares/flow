09bb8e6a1ecc2c8bd0452b36318bc8a6bfa4dfe4
Felipe Tavares <felipe.oltavares@gmail.com>
2016-02-08 01:58:04 -0300

    Uses port from env, if provided

--- src/interface/index.js
+++ src/interface/index.js
@@ -319,7 +319,7 @@ function renderBackground () {
 
 function server (data, done) {
   Util.compress(new Packet.Action(data, game.token), function (msg) {
-    socket.send(msg, 0, msg.length, 41322, 'ctrl-c.club', done);
+    socket.send(msg, 0, msg.length, 41322, 'flowmud.herokuapp.com', done);
   });
 }
 
